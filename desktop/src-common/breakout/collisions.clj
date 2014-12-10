f3ab37cc848fae60f76120843fd30900743d9dc0
nac67 <nac67@cornell.edu>
2014-12-10 13:10:39 -0500

    moduled out collision code

---
+++ desktop/src-common/breakout/collisions.clj
@@ -0,0 +1,41 @@
+(ns breakout.collisions)
+
+(defn colliding? [e1 e2]
+  (let [x1 (:x e1)
+        x2 (:x e2)
+        y1 (:y e1)
+        y2 (:y e2)
+        w1 (:w e1)
+        w2 (:w e2)
+        h1 (:h e1)
+        h2 (:h e2)
+        
+        cond1 (< x2 (+ x1 w1))
+        cond2 (< x1 (+ x2 w2))
+        cond3 (< y2 (+ y1 h1))
+        cond4 (< y1 (+ y2 h2))]
+    (and cond1 cond2 cond3 cond4)))
+
+
+(defn processCollisions [collider listOfObjects collFcn]
+  "Runs a collision check between collider entity and a list
+   of entities called listOfObjects. If there is a collision,
+   it will call collFcn on the entity in listOfObjects. It will return
+   [collision? newListOfObjects] where collision? is whether any collision
+   occured, and newListOfObjects is the list with collFcn applied to collied
+   objects.
+
+   collider - entity to run against all other entities
+   listOfObjects - list of entities to be tested against
+   collFcn - function of form (fn [entity] ...) which will be called on 
+      an entity in listOfObjects in event of a collision
+   
+   returns [didCollisionOccur? newListOfObjects]"
+  (reduce 
+    ; accumulator is whether a collision has occured yet, and newListOfObjects
+    (fn [[accColl? accList] entity]
+      (let [coll? (colliding? collider entity)
+            entity (if coll? (collFcn entity) entity)]
+        [(or coll? accColl?) (conj accList entity)]))
+    [false []]
+    listOfObjects))
--- desktop/src-common/breakout/core.clj
+++ desktop/src-common/breakout/core.clj
@@ -1,6 +1,7 @@
 (ns breakout.core
   (:require [play-clj.core :refer :all]
-            [play-clj.g2d :refer :all]))
+            [play-clj.g2d :refer :all]
+            [breakout.collisions :as collisions]))
 
 (def ball_path "ball.png")
 (def brick_path "brick.png")
@@ -60,49 +61,10 @@
            (bounce)
            (fallOffScreen)))
 
-(defn colliding? [e1 e2]
-  (let [x1 (:x e1)
-        x2 (:x e2)
-        y1 (:y e1)
-        y2 (:y e2)
-        w1 (:w e1)
-        w2 (:w e2)
-        h1 (:h e1)
-        h2 (:h e2)
-        
-        cond1 (< x2 (+ x1 w1))
-        cond2 (< x1 (+ x2 w2))
-        cond3 (< y2 (+ y1 h1))
-        cond4 (< y1 (+ y2 h2))]
-    (and cond1 cond2 cond3 cond4)))
-
-
-(defn processCollisions [collider listOfObjects collFcn]
-  "Runs a collision check between collider entity and a list
-   of entities called listOfObjects. If there is a collision,
-   it will call collFcn on the entity in listOfObjects. It will return
-   [collision? newListOfObjects] where collision? is whether any collision
-   occured, and newListOfObjects is the list with collFcn applied to collied
-   objects.
-
-   collider - entity to run against all other entities
-   listOfObjects - list of entities to be tested against
-   collFcn - function of form (fn [entity] ...) which will be called on 
-      an entity in listOfObjects in event of a collision
-   
-   returns [didCollisionOccur? newListOfObjects]"
-  (reduce 
-    ; accumulator is whether a collision has occured yet, and newListOfObjects
-    (fn [[accColl? accList] entity]
-      (let [coll? (colliding? collider entity)
-            entity (if coll? (collFcn entity) entity)]
-        [(or coll? accColl?) (conj accList entity)]))
-    [false []]
-    listOfObjects))
     
 (defn processBreakoutCollisions [ball paddle bricks]
     (let [[collisions? [paddle & bricks]] 
-            (processCollisions 
+            (collisions/processCollisions 
               ball 
               (cons paddle bricks) ; collide ball against paddle or bricks
               (fn [hitObject]
